63d99fc934098d170ad159a620303938cd76e6ad
aquasky <aquasky0323@gmail.com>
2015-09-25 22:36:34 +0900

    Scene to Class

--- main.js
+++ main.js
@@ -1,30 +1,54 @@
+var app;
+
 tm.main(function(){
-    var app = tm.display.CanvasApp("#world");   // canvasを取得
-    app.resize(640, 960);                       // サイズ
+    app = tm.display.CanvasApp("#world");   // canvasを取得
+    app.resize(640, 960);                   // サイズ
     app.background = "black";           // 背景色
     app.enableStats();                  // FPSを表示
     app.fitWindow();                    // 表示領域の設定
 
-    // タイトルシーンの生成
-    var titleScene = tm.app.Scene();
-    titleScene.update = function(){
-        console.log("titleScene");
-        // Zキーかタッチされた時にシーンを切替え
-        if (app.keyboard.getKeyDown("Z") || app.pointing.getPointingEnd()) {
-            app.replaceScene(mainScene);
-        }
-    }
-
-    // メインシーンの生成
-    var mainScene = tm.app.Scene();
-    mainScene.update = function(){
-        console.log("mainScene");
-        if (app.keyboard.getKeyDown("Z") || app.pointing.getPointingEnd()) {
-            app.replaceScene(titleScene);
-        }
-    }
-
-
-    app.replaceScene(titleScene);       // 最初のシーンを設定
+    app.replaceScene(TitleScene());     // 最初のシーンを設定
     app.run();                          // 実行
 });
+
+// タイトルシーン
+(function(ns){
+    ns.TitleScene = tm.createClass({
+        superClass: tm.app.Scene,   // 親クラス
+
+        // 初期化
+        init: function(){
+            this.superInit();
+        },
+
+        // 更新
+        update: function(){
+            console.log("TitleScene");
+            // Zキーかタッチされた時にシーンを切替え
+            if (app.keyboard.getKeyDown("Z") || app.pointing.getPointingEnd()) {
+                app.replaceScene(MainScene());
+            }
+        }
+    });
+})(window);
+
+// メインシーンの生成
+(function(ns){
+    ns.MainScene = tm.createClass({
+        superClass: tm.app.Scene,   // 親クラス
+
+        // 初期化
+        init: function(){
+            this.superInit();
+        },
+
+        // 更新
+        update: function(){
+            console.log("MainScene");
+            // Zキーかタッチされた時にシーンを切替え
+            if (app.keyboard.getKeyDown("Z") || app.pointing.getPointingEnd()) {
+                app.replaceScene(TitleScene());
+            }
+        }
+    });
+})(window);
