1292cb441d172be6cc84db8aca63b1f886a0f660
aquasky <aquasky0323@gmail.com>
2015-09-25 22:20:03 +0900

    Add Scene sample

--- main.js
+++ main.js
@@ -5,5 +5,26 @@ tm.main(function(){
     app.enableStats();                  // FPSを表示
     app.fitWindow();                    // 表示領域の設定
 
+    // タイトルシーンの生成
+    var titleScene = tm.app.Scene();
+    titleScene.update = function(){
+        console.log("titleScene");
+        // Zキーかタッチされた時にシーンを切替え
+        if (app.keyboard.getKeyDown("Z") || app.pointing.getPointingEnd()) {
+            app.replaceScene(mainScene);
+        }
+    }
+
+    // メインシーンの生成
+    var mainScene = tm.app.Scene();
+    mainScene.update = function(){
+        console.log("mainScene");
+        if (app.keyboard.getKeyDown("Z") || app.pointing.getPointingEnd()) {
+            app.replaceScene(titleScene);
+        }
+    }
+
+
+    app.replaceScene(titleScene);       // 最初のシーンを設定
     app.run();                          // 実行
 });
