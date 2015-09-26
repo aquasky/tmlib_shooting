636c2f91b6c17136f135443a686453f89eddea10
aquasky <aquasky0323@gmail.com>
2015-09-26 17:15:42 +0900

    Add Player & Enemy

--- main.js
+++ main.js
@@ -10,20 +10,23 @@ var LOADING_DEFAULT_PARAM = {
 
 // 画像
 var ASSETS = {
-    "circle" : "image/circle.png"
+    "circle" : "image/circle.png",
+    "player" : "image/player.png"
 };
 
 tm.main(function(){
     var app = tm.display.CanvasApp("#world");   // canvasを取得
     app.resize(SCREEN_WIDTH, SCREEN_HEIGHT);    // サイズ
     app.background = "black";           // 背景色
+    app.fps = 60;                       // FPSを設定
     app.enableStats();                  // FPSを表示
     app.fitWindow();                    // 表示領域の設定
 
     // 画像を読み込んでタイトルシーンへ遷移
     app.replaceScene(LoadingScene({
         assets: ASSETS,
-        nextScene: TitleScene
+//        nextScene: TitleScene
+        nextScene: MainScene
     }));
 
     app.run();                          // 実行
@@ -117,10 +120,10 @@ tm.define("TitleScene", {
         console.log("TitleScene");
 
         // 画像を表示
-        this.sprite = tm.display.Sprite("circle", 32, 32);
-        this.sprite.position.set(240, 360);
+        this.sprite = tm.display.Sprite("circle", 64, 64);
+        this.sprite.position.set(SCREEN_WIDTH / 2, 360);
         this.sprite.speed = 5;
-        this.sprite.addChildTo(this);
+        this.addChild(this.sprite);
     },
 
     // 更新
@@ -138,7 +141,7 @@ tm.define("TitleScene", {
     }
 });
 
-// メインシーンの生成
+// メインシーン
 tm.define("MainScene", {
     superClass: "tm.app.Scene",   // 親クラス
 
@@ -146,6 +149,23 @@ tm.define("MainScene", {
     init: function(){
         this.superInit();
         console.log("MainScene");
+
+        // 自機の生成
+        this.player = Player();
+        this.player.position.set(SCREEN_WIDTH / 2, 860);
+        this.addChild(this.player);
+
+        // 敵のグループ
+        this.enemyGroup = tm.display.CanvasElement();
+        this.enemyGroup.update = function(app){
+            // 30フレーム毎に敵を生成
+            if (app.frame % 30 == 0) {
+                var enemy = Enemy();
+                enemy.position.set(Math.rand(40, app.height - 40), -20);
+                this.addChild(enemy);
+            }
+        }
+        this.addChild(this.enemyGroup);
     },
 
     // 更新
@@ -153,6 +173,60 @@ tm.define("MainScene", {
         // Zキーかタッチされた時にシーンを切替え
         if (app.keyboard.getKeyDown("Z") || app.pointing.getPointingEnd()) {
             app.replaceScene(TitleScene());
+        }
+    }
+});
+
+// プレイヤー
+tm.define("Player", {
+    superClass: "tm.display.Sprite",
+
+    init: function(){
+        this.superInit("player", 64, 64);
+        console.log("Player");
+
+        this.speed = 0; // 速度
+        this.velocity = tm.geom.Vector2(0, 0);  // 移動ベクトル
+    },
+
+    update: function(app){
+        var angle = app.keyboard.getKeyAngle();
+        if (null != angle) {
+            console.log(angle);
+            this.velocity.setDegree(angle, 1);  // 角度からベクトル量を計算
+            this.velocity.y *= -1;
+            this.speed = 5;
+        }
+
+        // 移動させる
+        this.position.add(tm.geom.Vector2.mul(this.velocity, this.speed));
+        this.speed *= 0.7;    // 速度を少しずつ減衰させていく
+    }
+});
+
+// 敵
+tm.define("Enemy", {
+    superClass: "tm.display.Shape",
+
+    init: function(){
+        this.superInit();
+        this.setSize(80, 80);
+
+        var c = this.canvas;
+        c.setTransformCenter();
+        c.setLineStyle(1.5, "round", "round");
+        c.setColorStyle("white", "rgb(255, 50, 50)");
+        c.fillStar(0, 0, 20, 16, 0.6);
+        c.strokeStar(0, 0, 20, 16, 0.6);
+    },
+
+    update: function(app){
+        this.y += 2.5;
+        this.rotation -=4;
+
+        // 画面外に出たら自身を削除
+        if (this.y > (app.height + 40)) {
+            this.remove();
         }
     }
 });
